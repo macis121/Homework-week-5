6048967c6346811835fdf9adadb88bbfe908afdf
michelles bells <macis@cca.edu>
2017-05-08 11:12:13 -0700

    Add files via upload

---
+++ Sol Lewitt #584H.js
@@ -0,0 +1,20 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+
+	
+fill(125, 100, 125);
+	rect(0, 0, 200, 200);
+fill(150,255,155);
+rect(199,0,199,200);
+
+fill(245, 245, 235);
+	rect(0, 199, 200, 200);
+	
+fill(225, 150, 100);
+	rect(199, 199, 200, 200);
+
+}
